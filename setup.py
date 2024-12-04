65c755d4d421c386799c1cc47070fbee81507677
Ahmed546 <ahmedwaleed0546@gmail.com>
2024-12-04 20:33:42 +0500

    setup.py file added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Medical bot genAi',
+    version='1.0.0',
+    author='Waleed Ahmed',
+    author_email='ahmedwaleed003@gmail..com',
+    description='A medical chatbot using Natural Language Processing and Machine Learning',
+    url='https://github.com/waleedahmed/medical-bot-genAi',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[]
+)
